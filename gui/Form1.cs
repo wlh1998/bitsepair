d9698afd85135538a911fb088963b2dbfed9c9ca
zs420670989 <45817841+zs420670989@users.noreply.github.com>
2019-01-18 20:24:44 +0800

    Add files via upload

--- gui/Form1.cs
+++ gui/Form1.cs
@@ -13,15 +13,40 @@ namespace 四则运算1
 {
     public partial class Form1 : Form
     {
-        private int seconds = 20,right=0,sum=0;
+        private int seconds = 20,right=0,sum=0,flag=0;
+        private static string CmdPath = @"C:\Windows\System32\cmd.exe";
+        string output = "",cmd=@"calculator.exe -g 1000 qa.txt";
         string line_q,line_a;
-        StreamReader sr = new StreamReader(@"qa.txt");
+        StreamReader sr;
         public Form1()
         {
             InitializeComponent();
         }
+        public static void RunCmd(string cmd, out string output)
+        {
+            cmd = cmd.Trim().TrimEnd('&') + "&exit";//说明：不管命令是否成功均执行exit命令，否则当调用ReadToEnd()方法时，会处于假死状态
+            using (System.Diagnostics.Process p = new System.Diagnostics.Process())
+            {
+                p.StartInfo.FileName = CmdPath;
+                p.StartInfo.UseShellExecute = false;        //是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;   //接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;  //由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true;   //重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true;          //不显示程序窗口
+                p.Start();//启动程序
 
-        private void button_pause_Click(object sender, EventArgs e)
+                //向cmd窗口写入命令
+                p.StandardInput.WriteLine(cmd);
+                p.StandardInput.AutoFlush = true;
+
+                //获取cmd窗口的输出信息
+                output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();//等待程序执行完退出进程
+                p.Close();
+            }
+        }
+   
+    private void button_pause_Click(object sender, EventArgs e)
         {
             if(button_pause.Text=="暂停")
             {
@@ -39,10 +64,12 @@ namespace 四则运算1
 
         private void btn_start_Click(object sender, EventArgs e)
         {
-            if (button_pause.Text == "暂停")
+            if (button_pause.Text == "暂停"&&flag==0)
 
             {
-                timer1.Start();
+                flag = 1;
+                RunCmd(cmd, out output);
+                sr = new StreamReader(@"qa.txt");
                 if ((line_q = sr.ReadLine()) != null)
                 {
                     label_question.Text = line_q;
